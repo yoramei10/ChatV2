82235269388342f90b2e9fce487d9d84914cb4da
USER <yoramei10@gmail.com>
2021-04-20 10:53:08 +0300

    init project V2

---
+++ ChatAppV2/src/test/java/com/sherut/ChatAppV2/ChatAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherut.ChatAppV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
